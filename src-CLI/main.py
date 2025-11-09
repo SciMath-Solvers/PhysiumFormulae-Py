99f5e85a117f911ee6561a0e04786f1f5ab11e59
JavasciptBeast <142036809+JavasciptBeast@users.noreply.github.com>
2025-11-08 19:34:39 -0600

    Implement force, mass, and acceleration calculations
    Added force_mass_acceleration function to calculate force, mass, or acceleration based on user input.

--- src-CLI/main.py
+++ src-CLI/main.py
@@ -3,5 +3,27 @@ import asyncio
 
 async def main():
     await print("Task Finished with Return Code 0")
+    force_mass_acceleration()
+async def force_mass_acceleration():
+    a = input("Acceleration without units, leave blank if none")
+    async.sleep(1)
+    f = input("Force without units, leave blank if none")
+    async.sleep(1)
+    m = input("Mass without units, leave blank if none")
+    async.sleep(1)
+    solve= int(input("What do you want to solve for (1.force,2.mass,3.accelertion)"))
+    match solve:
+        case 1:
+            f=m*a
+            print(f"{f} N")
+        case 2:
+            m=f/a
+            print(f"{m} Kg")
+        case 3:
+            a=f/m
+            print(f"{a} m/s/s")
+            
+    
+    
 
 asyncio.run(main())
